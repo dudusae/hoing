8d9310ef9cea33893158c29ae1b08e6d83d8b037
dudusae <dudusae@gmail.com>
2020-01-06 11:16:18 +0900

    Add Overtime Timer

--- src/components/DoingTimer.js
+++ src/components/DoingTimer.js
@@ -4,36 +4,58 @@ import './../style.css';
 const setTwoDigit = num => {
   if (num < 10) {
     return '0' + num;
-  } else {
-    return num;
-  }
+  } else return num;
 };
 
+const timeToMmSs = time => {
+    const min = setTwoDigit(Math.floor(time / 60000));
+    const sec = setTwoDigit(Math.floor((time % 60000) / 1000));
+    return `${min}:${sec}`;
+  };
+
 const TimerOn = () => {
-  const [count, setCount] = useState(1500000);
+  //   const [count, setCount] = useState(1500000);
+  const [count, setCount] = useState(2000);
   const [isRunning, setIsRunning] = useState(true);
+  const [timeIn, setTimeIn] = useState(true);
+  const [timeOutCount, setTimeOutCount] = useState(1000);
 
   useInterval(
     () => {
       setCount(count - 1000);
+      if (count < 1999) {
+        setTimeIn(false);
+      }
     },
-    isRunning ? 1000 : null,
+    isRunning && timeIn ? 1000 : null,
   );
 
-  const min = setTwoDigit(Math.floor(count / 60000));
-  const sec = setTwoDigit(Math.floor((count % 60000) / 1000));
+  useInterval(
+    () => {
+      setTimeOutCount(timeOutCount + 1000);
+    },
+    !timeIn ? 1000 : null,
+  );
+
+  
 
   const handleIsRunningChange = e => {
     setIsRunning(!isRunning);
   };
 
   return (
-    <div className="doing__timer">
-      {min}:{sec}
-      <button onClick={handleIsRunningChange}>
-        {isRunning === true ? '일시정지' : '재개'}
-      </button>
-    </div>
+    <>
+      <div className="doing__timer">
+        {timeToMmSs(count)}
+        {timeIn ? 
+            <button onClick={handleIsRunningChange}>
+                {isRunning === true ? '일시정지' : '재개'}
+             </button> : ''}
+      </div>
+      <div>
+          {timeIn ? '' : timeToMmSs(timeOutCount)}
+      </div>
+    </>
   );
 };
 
